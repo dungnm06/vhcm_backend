97f7bcfaa4bba762542eebeebbc52d6757da9336
dungnm06 <dungnmse06067@fpt.edu.vn>
2020-10-20 22:10:07 +0900

    Fix SSL keys

--- vhcm/common/config/config_manager.py
+++ vhcm/common/config/config_manager.py
@@ -5,6 +5,24 @@ from vhcm.common.response_json import ResponseJSON
 from vhcm.common.singleton import Singleton
 
 
+class ConfigLoader(object, metaclass=Singleton):
+    def __init__(self, settings):
+        self.settings = settings
+
+    def get_setting_value(self, key):
+        setting = self.settings.filter(setting_id=key).first()
+        if setting is None:
+            raise Exception('Setting not found: ', key)
+
+        if setting.value:
+            return setting.value
+        else:
+            return setting.default
+
+    def get_setting(self, key):
+        return self.settings.filter(setting_id=key).first()
+
+
 # ALL CONFIGS
 VNCORENLP = 'vncorenlp'
 NAMED_ENTITY_TYPES = 'named_entity_types'
@@ -12,9 +30,9 @@ CRITICAL_DATA_NG_PATTERNS = 'critical_data_ng_pattern'
 EXCLUDE_POS_TAG = 'exclude_pos_tag'
 EXCLUDE_WORDS = 'exclude_word'
 
-#Instance
+# Instance
 SYSTEM_SETTINGS = SystemSetting.objects.all()
-CONFIG_LOADER = config.ConfigLoader(SYSTEM_SETTINGS)
+CONFIG_LOADER = ConfigLoader(SYSTEM_SETTINGS)
 
 
 @api_view(['GET'])
@@ -33,17 +51,17 @@ def add_system_settings(request):
          'Language Processing: Exclude POS-tag',
          'These POS-tag will be ignored when analyze sentence subjects and verbs in language processing phase (comma separated)',
          'E,A,L,CH,X',
-         '')
+         ''),
         ('named_entity_types',
          'Language Processing: Names entity types',
          'Accepted NER types for extracting main subjects in sentence',
          'LOC,PER,ORG,MISC',
-         '')
+         ''),
         ('subject_data_ng_pattern',
          'Language Processing: Bad subject structure patterns',
          'Using this to analyze main subjects in sentences is matching.\nInput pattern examples:\nX-main\nmain-X\nX-main-X\nIn that (main) is main subject, (X) is word types adjacent to main subject',
          'N-E-main,N-main',
-         '')
+         ''),
         ('exclude_word',
          'Language Processing: Exclude words',
          'These words will be ignored when analyze sentence subjects and verbs in language processing phase (comma separated)',
@@ -51,7 +69,8 @@ def add_system_settings(request):
          '')
     ]
 
-    settings = [SystemSetting(setting_id=s[0], setting_name=s[1], description=s[2], value=s[3], default=s[4]) for s in settings]
+    settings = [SystemSetting(setting_id=s[0], setting_name=s[1], description=s[2], value=s[3], default=s[4]) for s in
+                settings]
     SystemSetting.objects.all().delete()
     SystemSetting.objects.bulk_create(settings)
 
@@ -59,21 +78,3 @@ def add_system_settings(request):
     result.set_result_data(True)
     response.data = result.to_json()
     return response
-
-
-class ConfigLoader(object, metaclass=Singleton):
-    def __init__(self, settings):
-        self.settings = settings
-
-    def get_setting_value(self, key):
-        setting = self.settings.filter(setting_id=key).first()
-        if setting is None:
-            raise Exception('Setting not found: ', key)
-
-        if setting.value:
-            return setting.value
-        else:
-            return setting.default
-
-    def get_setting(self, key):
-        return self.settings.filter(setting_id=key).first()
