7d76afc137267f75488b1807baefe9b43cba15bf
TungDS <kirisaki124@yahoo.com>
2020-11-26 01:17:32 +0700

    API implements

--- vhcm/biz/nlu/vhcm_chatbot.py
+++ vhcm/biz/nlu/vhcm_chatbot.py
@@ -70,7 +70,7 @@ def init_bot():
 
         with open(version_file_path, 'w') as f:
             json.dump(version, f, indent=4)
-        # raise RuntimeError
+        raise RuntimeError
         # Intent classifier
         intent_classifier_instance = IntentClassifier()
         intent_classifier_instance.load()
